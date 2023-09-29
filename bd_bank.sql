136855e879ad56b7c96ff6d6310453c744a7c7dc
Fernando j <kolumbia3000@gmail.com>
2023-09-29 08:43:21 -0300

    Inicio do projeto/Criação do banco de dados

---
+++ bd_bank.sql
@@ -0,0 +1,34 @@
+-- Adminer 4.8.1 MySQL 5.5.5-10.5.21-MariaDB-1:10.5.21+maria~ubu2004 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP DATABASE IF EXISTS `bd_bank`;
+CREATE DATABASE `bd_bank` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `bd_bank`;
+
+DROP TABLE IF EXISTS `tb_cliente`;
+CREATE TABLE `tb_cliente` (
+  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_cliente` varchar(100) NOT NULL,
+  `senha_cliente` varchar(100) NOT NULL,
+  `cpf_cliente` varchar(50) NOT NULL,
+  `email_cliente` varchar(200) NOT NULL,
+  PRIMARY KEY (`id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+DROP TABLE IF EXISTS `tb_moeda`;
+CREATE TABLE `tb_moeda` (
+  `id_moeda` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_moeda` varchar(100) NOT NULL,
+  `valor_moeda` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_moeda`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- 2023-09-29 11:40:11
