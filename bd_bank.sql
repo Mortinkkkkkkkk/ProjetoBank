40c435980aeaaf009e3ecde766707ac3c2cf4262
Fernando J <kolumbia3000@gmail.com>
2023-10-02 15:17:59 -0300

    Adição da carteira

--- bd_bank.sql
+++ bd_bank.sql
@@ -1,4 +1,4 @@
--- Adminer 4.8.1 MySQL 5.5.5-10.5.21-MariaDB-1:10.5.21+maria~ubu2004 dump
+-- Adminer 4.8.1 MySQL 5.5.5-10.5.19-MariaDB-1:10.5.19+maria~ubu2004 dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -10,6 +10,20 @@ SET NAMES utf8mb4;
 DROP DATABASE IF EXISTS `bd_bank`;
 CREATE DATABASE `bd_bank` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `bd_bank`;
+
+DROP TABLE IF EXISTS `tb_carteira`;
+CREATE TABLE `tb_carteira` (
+  `id_carteira` int(11) NOT NULL,
+  `tb_cliente_id_cliente` int(11) NOT NULL,
+  `tb_moeda_id_moeda` int(11) NOT NULL,
+  `valor_total` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  KEY `tb_cliente_id_cliente` (`tb_cliente_id_cliente`),
+  KEY `tb_moeda_id_moeda` (`tb_moeda_id_moeda`),
+  CONSTRAINT `tb_carteira_ibfk_1` FOREIGN KEY (`tb_cliente_id_cliente`) REFERENCES `tb_cliente` (`id_cliente`),
+  CONSTRAINT `tb_carteira_ibfk_2` FOREIGN KEY (`tb_moeda_id_moeda`) REFERENCES `tb_moeda` (`id_moeda`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 
 DROP TABLE IF EXISTS `tb_cliente`;
 CREATE TABLE `tb_cliente` (
@@ -31,4 +45,4 @@ CREATE TABLE `tb_moeda` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
--- 2023-09-29 11:40:11
+-- 2023-10-02 18:16:34
