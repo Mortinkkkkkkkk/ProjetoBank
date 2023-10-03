81f04d49512fe85d7aed6d3e6c2134ff64ecca02
LimaTarcisio <116592238+Mortinkkkkkkkk@users.noreply.github.com>
2023-10-03 14:44:22 -0300

    alterações basicas no moedas

--- site/moedas.php
+++ site/moedas.php
@@ -8,12 +8,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
 </head>
 <body>
-    
-        
+    <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand">Sla</a>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit"><i class="bi bi-search"></i>Pesquisar</button>  
+            
+        </form>
+    </div>
+    </nav>
 
-Moedas em Destasque <br> <br>
+        
+ 
+    <div>
+        Moedas em Destasque 
+    </div>
+    <br> <br> 
+    
+
+<div>
 <?php
         $sql = "SELECT * FROM tb_moeda";
         $resultado = mysqli_query($conexao,$sql);
@@ -37,5 +56,9 @@ Moedas em Destasque <br> <br>
             }
         }
     ?>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
 </body>
 </html>
