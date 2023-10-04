6d9dc16df152adac3e33cc860a2467470389220e
Fernando J <Kolumbia3000@gmail.com>
2023-10-04 15:17:28 -0300

    Integração do randomizer

--- site/carteira.php
+++ site/carteira.php
@@ -7,26 +7,40 @@
 </head>
 <body>
     <?php
-        $conexao = mysqli_connect("db","root","123","bd_bank");
-        $sql = "SELECT * FROM tb_carteira";
-        $resultado = mysqli_query($conexao,$sql);
-        if (mysqli_num_rows($resultado) > 0) {
-            while ($row = mysqli_fetch_assoc($resultado)){
-                $idcli = $row['tb_cliente_id_cliente'];
-                $idmoeda = $row['tb_moeda_id_moeda'];
-                $quantidade = $row['quantidade'];
-                $selectcli = "SELECT nome_cliente FROM tb_cliente WHERE id_cliente = $idcli";
-                $selectmoeda = "SELECT * FROM tb_moeda WHERE id_moeda = $idmoeda";
-                $cliente = mysqli_query($conexao,$selectcli);
-                $moeda = mysqli_query($conexao,$selectmoeda);
-                $linhacli = mysqli_fetch_assoc($cliente);
-                $nomecli = $linhacli['nome_cliente'];
-                $linhamoeda = mysqli_fetch_assoc($moeda);
-                $nomemoeda = $linhamoeda['nome_moeda'];
-                $valormoeda = $linhamoeda['valor_moeda'];
-                echo $nomecli . "<br>";
-                echo $nomemoeda . "<br>";
-                echo $valormoeda * $quantidade . "<br>";
+        require_once "conexao.php";
+        require_once "porcentagem_aleatoria.php";
+
+        $mais_menos = 0;
+        if (rand(0,100) == 100) {
+            $porcentagem_aleatoria = $vetor_de_porcentagens_maior_que_dois_porcento[rand(0,24)];
+
+          }else {
+            $porcentagem_aleatoria = $vetor_de_porcentagens_menor_que_dois_porcento[rand(0,82)];
+          }
+          while ($mais_menos == 0) {
+            $mais_menos = rand(-1,1);
+          }
+
+          $sql = "SELECT * FROM tb_carteira";
+          $resultado = mysqli_query($conexao,$sql);
+          if (mysqli_num_rows($resultado) > 0) {
+              while ($row = mysqli_fetch_assoc($resultado)){
+                  $idcli = $row['tb_cliente_id_cliente'];
+                  $idmoeda = $row['tb_moeda_id_moeda'];
+                  $quantidade = $row['quantidade'];
+                  $selectcli = "SELECT nome_cliente FROM tb_cliente WHERE id_cliente = $idcli";
+                  $selectmoeda = "SELECT * FROM tb_moeda WHERE id_moeda = $idmoeda";
+                  $cliente = mysqli_query($conexao,$selectcli);
+                  $moeda = mysqli_query($conexao,$selectmoeda);
+                  $linhacli = mysqli_fetch_assoc($cliente);
+                  $nomecli = $linhacli['nome_cliente'];
+                  $linhamoeda = mysqli_fetch_assoc($moeda);
+                  $nome_moeda = $linhamoeda['nome_moeda'];
+                  $valor_moeda = $linhamoeda['valor_moeda'];
+                  $calculo_valor_atual_moeda = $valor_moeda + $mais_menos * ($valor_moeda * $porcentagem_aleatoria) ;
+                  echo $nomecli . "<br>";
+                  echo $nome_moeda . "<br>";
+                  echo $calculo_valor_atual_moeda * $quantidade . "<br>";
             }
         }
     
