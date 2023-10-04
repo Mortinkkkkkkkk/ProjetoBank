9e1fdda14b6a0052cb15b8421401eef2d53bc9ed
Fernando J <Kolumbia3000@gmail.com>
2023-10-04 16:17:25 -0300

    Atualização do banco

--- bd_bank.sql
+++ bd_bank.sql
@@ -1,4 +1,4 @@
--- Adminer 4.8.1 MySQL 5.5.5-10.5.20-MariaDB-1:10.5.20+maria~ubu2004 dump
+-- Adminer 4.8.1 MySQL 5.5.5-10.5.22-MariaDB-1:10.5.22+maria~ubu2004 dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -16,7 +16,6 @@ CREATE TABLE `tb_carteira` (
   `id_carteira` int(11) NOT NULL,
   `tb_cliente_id_cliente` int(11) NOT NULL,
   `tb_moeda_id_moeda` int(11) NOT NULL,
-  `valor_total` int(11) NOT NULL,
   `quantidade` int(11) NOT NULL,
   KEY `tb_cliente_id_cliente` (`tb_cliente_id_cliente`),
   KEY `tb_moeda_id_moeda` (`tb_moeda_id_moeda`),
@@ -24,6 +23,11 @@ CREATE TABLE `tb_carteira` (
   CONSTRAINT `tb_carteira_ibfk_2` FOREIGN KEY (`tb_moeda_id_moeda`) REFERENCES `tb_moeda` (`id_moeda`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `tb_carteira` (`id_carteira`, `tb_cliente_id_cliente`, `tb_moeda_id_moeda`, `quantidade`) VALUES
+(1,	1,	1,	10),
+(2,	1,	2,	50),
+(3,	1,	3,	10),
+(4,	2,	3,	4);
 
 DROP TABLE IF EXISTS `tb_cliente`;
 CREATE TABLE `tb_cliente` (
@@ -35,6 +39,10 @@ CREATE TABLE `tb_cliente` (
   PRIMARY KEY (`id_cliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `tb_cliente` (`id_cliente`, `nome_cliente`, `senha_cliente`, `cpf_cliente`, `email_cliente`) VALUES
+(1,	'fulano',	'123',	'789456325656',	'fulano@gmail.com'),
+(2,	'teste',	'456',	'123456789',	'teste@gmail.com'),
+(3,	'gugu',	'789',	'64656454658',	'gugu@gugu.com');
 
 DROP TABLE IF EXISTS `tb_moeda`;
 CREATE TABLE `tb_moeda` (
@@ -55,4 +63,4 @@ INSERT INTO `tb_moeda` (`id_moeda`, `nome_moeda`, `sigla_moeda`, `valor_moeda`) 
 (7,	'LiteCoin',	'LTC',	336.37),
 (8,	'Santos FC Fan Token',	'SANTOS',	15);
 
--- 2023-10-03 16:18:59
+-- 2023-10-04 19:16:46
