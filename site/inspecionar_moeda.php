03c26024b2177f58f7863b9eb6e33b69eeb6e2f7
LimaTarcisio <tarcisio.pesquisa.estudo@gmail.com>
2023-10-04 13:57:44 -0300

    adição de páginas que futuramente serao uteis

---
+++ site/inspecionar_moeda.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    ?>
+    <form action="compra_de_moeda.php">
+        <button>
+            Comprar
+        </button>
+    </form>
+</body>
+</html>
