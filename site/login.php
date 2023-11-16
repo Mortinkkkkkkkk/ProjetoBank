b613adb6bdd0536cc509f12351bb26c3e08443af
LimaTarcisio <tarcisio.pesquisa.estudo@gmail.com>
2023-11-16 10:12:22 -0300

    sla

--- site/login.php
+++ site/login.php
@@ -1,19 +1,5 @@
-<?php
-session_start();
-require_once 'conexao.php';
-if (isset($_SESSION['logado'])) {
-    $logado = "";
-    $login = "hidden";
-} else {
-    $logado = "hidden";
-    $login = "";
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -28,87 +14,103 @@ if (isset($_SESSION['logado'])) {
 
     <!-- Script de icones -->
     <script src="https://kit.fontawesome.com/bc42253982.js" crossorigin="anonymous"></script>
-    
+
     <title>Document</title>
-</head>
-
 <body>
-<nav class="navbar navbar-expand-lg" style="background-color: #e3f2fd;">
+    <nav class="navbar navbar-expand-lg" style="background-color:  #0b6e20; border-radius: 32px 64px; margin-right: 100px; margin-left: 100px">
+              
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php"><img
-                    src="../img/Firefly logo minimalista para um banco digital de criptomoedas com o tema verde 62636.jpg"
-                    alt="imagem" height="50px" width="50px" style="border-radius: 40px"></a>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link">
-                            <form action="carteira.php">
-                                <button class="btn icon-link-hover" type="submit" <?php echo $logado; ?>
+                <a class="navbar-brand" href="index.php"><ion-icon name="wallet-outline" size ="large" style="color: lightgreen;"></ion-icon></a></a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link">
+                                <form action="carteira.php">
+                                    <button class="btn text-white icon-link-hover" type="submit" <?php echo $logado; ?>
+                            style='--bs-icon-link-transform: translate3d(0, -.125rem, 0); color: black; border: 0px;'>
+                            <i class="fa-solid fa-wallet fa-lg bi"></i> Carteira</button>
+                        </form>
+                    </a> 
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page">
+                        <form action="moedas.php">
+                            <button type="submit" class="btn icon-link-hover"
                                 style='--bs-icon-link-transform: translate3d(0, -.125rem, 0); color: black; border: 0px;'>
-                                <i class="fa-solid fa-wallet fa-lg bi"></i> Carteira</button>
-                            </form>
-                        </a> 
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page">
-                            <form action="moedas.php">
-                                <button type="submit" class="btn icon-link-hover"
-                                    style='--bs-icon-link-transform: translate3d(0, -.125rem, 0); color: black; border: 0px;'>
-                                    <i class="fa-solid fa-coins fa-lg bi"></i> Moedas</button>
-                            </form>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page">
-                            <form action="login.php">
-                                <button type="submit" class="btn icon-link-hover" <?php echo $login; ?>
-                                style='--bs-icon-link-transform: translate3d(0, -.125rem, 0); color: black; border: 0px;'>
-                                <i class="fa-solid fa-user-check fa-lg bi"></i> Login</button>
-                            </form>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page">
-                            <form action="cadastro.php">
-                                <button type="submit" class="btn icon-link-hover" <?php echo $login; ?> 
-                                style='--bs-icon-link-transform: translate3d(0, -.125rem, 0); color: black; border: 0px;'>
-                                <i class="fa-solid fa-user-plus fa-lg bi"></i> Cadastro</button>
-                            </form>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link">
-                            <form action="carrinho.php">
-                                <button type="submit" class="btn icon-link-hover"<?php echo $logado; ?>
-                                style='--bs-icon-link-transform: translate3d(0, -.125rem, 0); color: black; border: 0px;'>
-                                <i class="fa-solid fa-cart-shopping fa-lg bi"></i> Carrinho</button>
-                            </form>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link">
-                            <form action="logout.php">
-                                <button type="submit" class="btn icon-link-hover"<?php echo $logado; ?>
-                                style='--bs-icon-link-transform: translate3d(0, -.125rem, 0); color: black; border: 0px;'>
-                                <i class="fa-solid fa-arrow-right-from-bracket fa-lg bi"></i> Log-out</button>
-                            </form>
-                        </a>
-                    </li>
-                </div>
+                                <i class="fa-solid fa-coins fa-lg bi"></i> Moedas</button>                                        
+                                </form>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page">
+                        <form action="login.php">
+                            <button type="submit" class="btn icon-link-hover" <?php echo $login; ?>
+                            style='--bs-icon-link-transform: translate3d(0, -.125rem, 0); color: black; border: 0px;'>
+                            <i class="fa-solid fa-user-check fa-lg bi"></i> Login</button>
+                        </form>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page">
+                        <form action="cadastro.php">
+                            <button type="submit" class="btn icon-link-hover" <?php echo $login; ?> 
+                            style='--bs-icon-link-transform: translate3d(0, -.125rem, 0); color: black; border: 0px;'>
+                            <i class="fa-solid fa-user-plus fa-lg bi"></i> Cadastro</button>
+                        </form>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">
+                        <form action="carrinho.php">
+                            <button type="submit" class="btn icon-link-hover"<?php echo $logado; ?>
+                            style='--bs-icon-link-transform: translate3d(0, -.125rem, 0); color: black; border: 0px;'>
+                            <i class="fa-solid fa-cart-shopping fa-lg bi"></i> Carrinho</button>
+                        </form>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">
+                        <form action="logout.php">
+                            <button type="submit" class="btn icon-link-hover"<?php echo $logado; ?>
+                            style='--bs-icon-link-transform: translate3d(0, -.125rem, 0); color: black; border: 0px;'>
+                            <i class="fa-solid fa-arrow-right-from-bracket fa-lg bi"></i> Log-out</button>
+                        </form>
+                    </a>
+                </li>
+                
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page">
+                        <form action="cadastro.html">
+                            <button type="submit" class="btn text-white ">Cadastro</button>
+
+                        </form>
+                    </a>
+                    
+                    
                 </ul>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                </div>
+
+           
             </div>
+          
+            
+            
+        </div>
     </nav>
+    
+    
+</li>
 
+</div>
+</nav>
+    
+</li>
 
-
+</div>
+</nav>
     <div class="container mt-5">
         <h1>Login no Banco</h1> <br><br> <br>
     
-    <form action="fazer_login.php">
+    <form action="login.php">
     
         <div class="form-floating">
             <input class="form-control" name="email_login" placeholder="Email" type="text" id="email_login"> <br><br>
@@ -122,10 +124,13 @@ if (isset($_SESSION['logado'])) {
         <label for="senha_login">Senha</label>
     </div>
     
-        <button class="btn btn-success">Entrar</button>
+        <button class="btn btn-success">Cadastrar</button>
     </div> <br><br>
     </form>
 
-
+    <script script  type = "module"  src = "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js" > </script> 
+    <script script  nomodule  src = "https://unpkg .com/ionicons@7.1.0/dist/ionicons/ionicons.js" > </script>
+    
+</body>
 
    
